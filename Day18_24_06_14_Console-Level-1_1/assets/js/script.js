49afa62466e07044f17d20e573d497c38baf6c61
René Behrens <rene.behrens@aol.com>
2024-06-14 12:01:02 +0200

    done task Console-Level-1_1

---
+++ Day18_24_06_14_Console-Level-1_1/assets/js/script.js
@@ -0,0 +1,34 @@
+// 1. Beispiel
+
+let a = 12;
+let b = a * 3;
+console.log(b);
+
+// 2.
+
+let helloWorld = "Hello World";
+console.log(helloWorld);
+
+let firstName = "Anton";
+let age = 28;
+
+console.lo = firstName + " " + age;
+
+//3.
+
+let job = "Trainer:in";
+console.log(job);
+
+let married = true;
+console.log(married);
+
+//4.
+console.log(
+  firstName +
+    " ist " +
+    age +
+    " Jahre alt, von Beruf " +
+    job +
+    " und ist verheiratet: " +
+    married
+);
