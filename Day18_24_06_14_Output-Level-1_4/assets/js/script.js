0037396e36f1d07a4980678561a79bee85841546
René Behrens <rene.behrens@aol.com>
2024-06-14 14:29:19 +0200

    done task Output-Level-1_4

---
+++ Day18_24_06_14_Output-Level-1_4/assets/js/script.js
@@ -0,0 +1,38 @@
+//1.
+
+//window.alert("Hallo Welt");
+
+//2.
+
+// let text = window.prompt("Bitte geben Sie Ihren Namen ein.");
+// console.log(text);
+
+//3.
+
+// let text = window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+// console.log(text);
+
+//4.
+
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+
+//5.
+
+// let alter = window.prompt("Gib mir bitte dein Alter:");
+// console.log(alter);
+
+//6.
+
+// let b = 5;
+// let a = b;
+
+//7.
+
+// a = b - 3;
+// window.alert(a);
+
+//8.
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+// let grosse = "2,72m";
+// window.confirm(mann + grosse);
