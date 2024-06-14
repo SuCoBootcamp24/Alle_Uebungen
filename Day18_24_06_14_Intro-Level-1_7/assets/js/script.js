f6b625604d613b09ddc5bb0097f6d0570f0dfeb4
René Behrens <rene.behrens@aol.com>
2024-06-14 11:45:59 +0200

    done task Intro-Level-1_7

---
+++ Day18_24_06_14_Intro-Level-1_7/assets/js/script.js
@@ -0,0 +1,24 @@
+let helloworld = "Hello World";
+const pi = 3.14;
+const burjdubai = "828m";
+
+let fullname = "Jan Schmidt";
+const eifeltower = "324m";
+let camelcase = "camelCase";
+
+let kursstatus = true;
+
+let summe = 1234;
+// -------------------
+
+console.log(helloworld);
+helloworld = "Bye bye";
+
+console.log(pi);
+
+console.log(summe);
+
+console.log("kleine berechnung");
+
+console.log(pi + " * " + summe + ":");
+console.log(pi * summe);
