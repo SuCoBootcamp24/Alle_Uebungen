b7ecc421e581fbabdb7d2dfaa42c7f9090426544
René Behrens <rene.behrens@aol.com>
2024-06-17 14:04:56 +0200

    done task Intro-Level-1_15

---
+++ Day18_24_06_14_Intro-Level-1_15/assets/js/script.js
@@ -0,0 +1,36 @@
+//1.
+
+let num = 5;
+let anotherNum = 8;
+
+//2.
+
+console.log(num == anotherNum);
+
+//3.
+
+console.log(num === anotherNum);
+
+//4.
+
+console.log(num > anotherNum);
+
+//5.
+
+console.log(num < anotherNum);
+
+//6.
+
+console.log(num >= anotherNum);
+
+//7.
+
+console.log(num <= anotherNum);
+
+//8.
+
+console.log(num == "5");
+
+console.log(num != 8);
+
+console.log(num != "5");
