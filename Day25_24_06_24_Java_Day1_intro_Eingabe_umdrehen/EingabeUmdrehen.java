3a3bd2cfcd370a1902e5f501cd97b0844d18b197
René Behrens <rene.behrens@aol.com>
2024-06-24 15:01:03 +0200

    update task EingabeUmdrehen

--- Day25_24_06_24_Java_Day1_intro_Eingabe_umdrehen/EingabeUmdrehen.java
+++ Day25_24_06_24_Java_Day1_intro_Eingabe_umdrehen/EingabeUmdrehen.java
@@ -1,25 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class EingabeUmdrehen {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        ArrayList<String> wordArray = new ArrayList<String>();
-
-
-        System.out.println("Wörter mit enter eingeben:");
-        
+    public static void main(String[] args){
+         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> wordArray = new ArrayList<>();
         String line;
 
-       for (int i = 0; i < 3; i++) {
-        line = reader.readLine();
-        wordArray.add(line);
-       }
+        System.out.println("Geben Sie Ihre einzelnen Wörter ein (nur Enter zum Beenden)");
 
+        while (true) {
+            line = scanner.nextLine();
+            if (line.isEmpty()) {
+                scanner.close();
+                break;
+            }
+            wordArray.add(line);
+        }
+
+        System.out.println("________________________________");
         for (int i = wordArray.size(); i > 0; i--) {
         System.out.println(wordArray.get(i - 1));
         }
