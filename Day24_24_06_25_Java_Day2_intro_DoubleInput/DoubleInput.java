f512d0c1c272ae1c76bbc9b214ecfb5d24ceb041
René Behrens <rene.behrens@aol.com>
2024-06-25 11:03:52 +0200

    task done

---
+++ Day24_24_06_25_Java_Day2_intro_DoubleInput/DoubleInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DoubleInput {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
+        System.out.print(number + " * 2 = ");
+        System.out.println(number * 2);
+    }
+}
