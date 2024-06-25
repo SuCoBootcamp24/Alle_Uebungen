8a2cc67d9181bcd48927ad16b20ddf63da0e4669
René Behrens <rene.behrens@aol.com>
2024-06-25 17:10:14 +0200

    update and done

--- Day26_24_06_25_Java_Day2_Berechnung_von_Ausdrücken_in_Java/Calc.java
+++ Day26_24_06_25_Java_Day2_Berechnung_von_Ausdrücken_in_Java/Calc.java
@@ -10,42 +10,42 @@ public class Calc {
         double double_ergebnis;
 
         System.out.println("1.");
-        int_ergebnis = c - a * 3;  
+        int_ergebnis = c - a * 3;  //möglich (-3)
         System.out.println(int_ergebnis);
         System.out.println("");
 
         System.out.println("2.");
-        int_ergebnis = c / a ;  
+        int_ergebnis = c / a ;  //möglich (2)
         System.out.println(int_ergebnis);
         System.out.println("");
         
         System.out.println("3.");
-        int_ergebnis = c / b;  
+        int_ergebnis = c / b;  //möglich (1)
         System.out.println(int_ergebnis);
         System.out.println("");
         
         System.out.println("4.");
-        int_ergebnis = a + b / c; 
+        int_ergebnis = a + b / c; //möglich (3)
         System.out.println(int_ergebnis);
         System.out.println("");
         
         System.out.println("5.");
-        int_ergebnis = a++;
+        int_ergebnis = a++; //möglich a = 3    wird dann mit 1 addiert
         System.out.println(int_ergebnis);
         System.out.println("");
         
         System.out.println("6.");
-        int_ergebnis = ++a;
+        int_ergebnis = ++a; //möglich a = 5    wird erst mit 1 addiert
         System.out.println(int_ergebnis);
         System.out.println("");
         
         System.out.println("7.");
-        int_ergebnis = b + a--;
+        int_ergebnis = b + a--;   // möglich (10) nach der berechnung wird a um 1 reduziert (4)
         System.out.println(int_ergebnis);
         System.out.println("");
         
         System.out.println("8.");
-        int_ergebnis = b + --a;
+        int_ergebnis = b + --a;  // möglich (8) Vor der berechnung wird a um 1 reduziert (3)
         System.out.println(int_ergebnis);
         System.out.println("");
         
@@ -54,27 +54,27 @@ public class Calc {
         System.out.println("");
 
         System.out.println("10.");
-        double_ergebnis = c / b;
+        double_ergebnis = c / b;  //möglich (1.0)
         System.out.println(double_ergebnis);
         System.out.println("");
         
         System.out.println("11.");
-        double_ergebnis = x + y * b;
+        double_ergebnis = x + y * b;  //möglich (13.0)
         System.out.println(double_ergebnis);
         System.out.println("");
         
         System.out.println("12.");
-        double_ergebnis = (x + y) * b;
+        double_ergebnis = (x + y) * b; //möglich (19.0)
         System.out.println(double_ergebnis);
         System.out.println("");
         
         System.out.println("13.");
-        double_ergebnis = c + a / b;
+        double_ergebnis = c + a / b;  //möglich (6.0)
         System.out.println(double_ergebnis);
         System.out.println("");
-        
+        double_ergebnis = 0;
         System.out.println("14.");
-        double_ergebnis =  (x * c - a);
+        double_ergebnis =  (x * c - a); //möglich (6.0) die klammern sind aber überflüssig wenn nichts weiter gemacht werden soll
         System.out.println(double_ergebnis);
         System.out.println("");
         
