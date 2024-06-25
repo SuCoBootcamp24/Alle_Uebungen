2568cf82e9fa64c829ae362bf49e61a6c218e4f2
René Behrens <rene.behrens@aol.com>
2024-06-25 11:05:05 +0200

    update

--- Day24_24_06_25_Java_Day2_intro_DoubleInput/DoubleInput.java
+++ Day24_24_06_25_Java_Day2_intro_DoubleInput/DoubleInput.java
@@ -5,7 +5,7 @@ public class DoubleInput {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
-
+        input.close();
         System.out.print(number + " * 2 = ");
         System.out.println(number * 2);
     }
