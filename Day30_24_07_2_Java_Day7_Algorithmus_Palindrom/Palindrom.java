5652c756d518d094038e6490124e2fb3943e1790
René Behrens <rene.behrens@aol.com>
2024-07-03 10:15:08 +0200

    ignore space etc.

--- Day30_24_07_2_Java_Day7_Algorithmus_Palindrom/Palindrom.java
+++ Day30_24_07_2_Java_Day7_Algorithmus_Palindrom/Palindrom.java
@@ -1,7 +1,7 @@
 public class Palindrom {
     public static void main(String[] args) {
-        String text = "Hallo";
-        String otherString = "Otto";
+        String text = "amok oma";
+        String otherString = "abc";
 
         System.out.println(checkPalindrom(text));
         System.out.println(checkPalindrom(otherString));
@@ -9,8 +9,9 @@ public class Palindrom {
     }
 
     private static boolean checkPalindrom(String input) {
-        input = input.toLowerCase();
+        input = input.replaceAll("\\W","").toLowerCase();
         String inputReverse = new StringBuffer(input).reverse().toString();
+    
 
         if (input.equals(inputReverse)) return true;
         return false;
