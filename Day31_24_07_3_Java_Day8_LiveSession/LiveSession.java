1423e88f5b45444dc6c93b4b70c6fcb5e33bf555
René Behrens <rene.behrens@aol.com>
2024-07-03 11:36:38 +0200

    Done Challenge 1. Split method

---
+++ Day31_24_07_3_Java_Day8_LiveSession/LiveSession.java
@@ -0,0 +1,35 @@
+
+import java.util.Arrays;
+
+public class LiveSession {
+    public static void main(String[] args) {
+        // LiveSession Challenge 1. create a logic for the method toLowerCase
+
+        String input = "Hallo Freunde!";
+        String[] test = split(input, "a");
+        System.out.println(Arrays.toString(test));
+    }
+
+    public static String[] split(String input, String delimiter) {
+        int partcounter = 1;
+        int index = 0;
+        while ((index = input.indexOf(delimiter, index)) != -1) {
+            partcounter++;
+            index += delimiter.length();
+        }
+        String[] resultArray = new String[partcounter];
+        int partIndex = 0;
+        int start = 0;
+        int end;
+
+        while ((end = input.indexOf(delimiter, start)) != -1) {
+            resultArray[partIndex] = input.substring(start, end);
+            partIndex++;
+            start = end + delimiter.length();
+        }
+
+        resultArray[partIndex] = input.substring(start);
+
+        return resultArray;
+    }
+}
