faabb48b7d8d1e8a2ab5402466c41634d35aea9c
René Behrens <rene.behrens@aol.com>
2024-07-04 13:16:35 +0200

    done task quersumme

--- Day31_24_07_4_Java_Day9_Fakulteat_einer_Zahl_berechnen/math.java
+++ Day31_24_07_4_Java_Day9_Fakulteat_einer_Zahl_berechnen/math.java
@@ -1,6 +1,7 @@
 public class math {
     public static void main(String[] args) {
-        System.out.println(powerOfNumber(8, 10));
+        System.out.println(quersumme(1456789));
+
     }
 
     public static int faculty(int n){
@@ -20,7 +21,18 @@ public class math {
     }
 
     public static int powerOfNumber(int x, int n) {
+        if (x < 0) return 0;
         if (n == 0) return 1;
         return x * powerOfNumber(x , n -1);
+    }
+
+    public static int quersumme(int number) {
+        if (number < 10) {
+            return number;
+        }                                   // Basisfall: Wenn die Zahl kleiner als 10 ist, ist ihre Quersumme einfach die Zahl selbst.
+        
+        int lastDigit = number % 10;        // Letzte Ziffer der Zahl berechnen (Modulo 10)
+        int restNumber = number / 10;       // Rekursiver Aufruf für den Rest der Zahl
+        return lastDigit + quersumme(restNumber);
     }
 }
