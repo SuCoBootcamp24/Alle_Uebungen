1ee846aec5ee22cfd41473660f61b54f2a888bfc
René Behrens <rene.behrens@aol.com>
2024-07-04 12:45:38 +0200

    done task sumNatureNumbers

--- Day31_24_07_4_Java_Day9_Fakulteat_einer_Zahl_berechnen/math.java
+++ Day31_24_07_4_Java_Day9_Fakulteat_einer_Zahl_berechnen/math.java
@@ -1,6 +1,6 @@
 public class math {
     public static void main(String[] args) {
-        System.out.println(faculty(5));
+        System.out.println(sumNatureNumbers(0));
     }
 
     public static int faculty(int n){
@@ -8,5 +8,14 @@ public class math {
             return 1;
         }
         return n * faculty(n - 1);
+    }
+
+    public static int sumNatureNumbers(int n) {
+        return sumNatureNumbers(1, n);
+    }
+    
+    public static int sumNatureNumbers(int index, int n) {
+        if (index >= n) return n;
+        return index + sumNatureNumbers(index + 1, n);
     }
 }
