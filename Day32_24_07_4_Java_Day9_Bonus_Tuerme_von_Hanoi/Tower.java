ee0d79926312dcb7d39b27a9e25263e536269985
René Behrens <rene.behrens@aol.com>
2024-07-04 13:56:09 +0200

    done bonus task hanoiTower

--- Day32_24_07_4_Java_Day9_Bonus_Tuerme_von_Hanoi/Tower.java
+++ Day32_24_07_4_Java_Day9_Bonus_Tuerme_von_Hanoi/Tower.java
@@ -1,3 +1,17 @@
 public class Tower {
+    public static void main(String[] args) {
+        int n = 4;
+        hanoiTower(n, 'A', 'B', 'C');
+    }
+
+    public static void hanoiTower(int n, char start, char mid, char end) {
+        if (n == 1) {
+            System.out.println("Verschiebe Scheibe " + n + " von " + start + " nach " + end);
+        } else {
+            hanoiTower(n-1, start, end, mid);
+            System.out.println("Verschiebe Scheibe " + n + " von " + start + " nach " + end);
+            hanoiTower(n-1, mid, start, end);
+        }
+    }
     
 }
