c28aefc819a236e42e2a09112ad1e07571274e7e
René Behrens <rene.behrens@aol.com>
2024-07-31 12:38:09 +0200

    done task 2. removeDublicates

--- Day51_24_07_31_Java_Day25_Ueberpruefung_Liste_auf_eindeutige_Elemente/src/main/java/de/supercode/Checker.java
+++ Day51_24_07_31_Java_Day25_Ueberpruefung_Liste_auf_eindeutige_Elemente/src/main/java/de/supercode/Checker.java
@@ -8,4 +8,9 @@ public class Checker {
     public static boolean hasUniceElements(List list) {
         return list.size() == new java.util.HashSet<>(list).size();
     }
+
+
+    public static List removeAllDuplicates(List intList) {
+        return new java.util.ArrayList<>(new java.util.HashSet<>(intList));
+    }
 }
--- Day51_24_07_31_Java_Day25_Ueberpruefung_Liste_auf_eindeutige_Elemente/src/main/java/de/supercode/Main.java
+++ Day51_24_07_31_Java_Day25_Ueberpruefung_Liste_auf_eindeutige_Elemente/src/main/java/de/supercode/Main.java
@@ -11,5 +11,15 @@ public class Main {
         List list = new ArrayList(Arrays.asList(1, 2, 3, 4, 5, 5));
 
         System.out.println(Checker.hasUniceElements(list));
+
+        List<Integer> intList = Arrays.asList(1, 2, 3, 4, 2, 3, 5, 7, 9, 4);
+
+        List result = Checker.removeAllDuplicates(intList);
+        System.out.println(result);
+
+
+
+
+
     }
 }
