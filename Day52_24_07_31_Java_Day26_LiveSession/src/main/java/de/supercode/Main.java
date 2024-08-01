ebd5e1267177f220d485ae7092b61e66b8333cde
René Behrens <rene.behrens@aol.com>
2024-08-01 11:14:28 +0200

    Update Main.java

--- Day52_24_07_31_Java_Day26_LiveSession/src/main/java/de/supercode/Main.java
+++ Day52_24_07_31_Java_Day26_LiveSession/src/main/java/de/supercode/Main.java
@@ -12,7 +12,13 @@ public class Main {
 
         while (true) {
             System.out.println("Bitte gebe einen Namen, \"LISTE\" (Zur Ausgabe aller Namen) oder zum Beenden \"ENDE\" ein:");
-            String userInput = scanner.nextLine().trim();
+            String userInput = scanner.nextLine();
+
+            // Überprüfung, ob die Eingabe nur Buchstaben enthält
+            if (!userInput.matches("[a-zA-ZäöüÄÖÜß]+") && !userInput.equals("LISTE") && !userInput.equals("ENDE")) {
+                System.out.println("Bitte gebe nur gültige Namen ohne Zahlen ein.");
+                continue;
+            }
 
             if (userInput.equals("LISTE")) {
                 System.out.println(namelist.isEmpty() ? "Keine Namen in der Liste." : "Namen in der Liste:\n" + String.join("\n", namelist));
