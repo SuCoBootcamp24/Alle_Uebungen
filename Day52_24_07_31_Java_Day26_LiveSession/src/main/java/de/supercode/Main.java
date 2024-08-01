9c4a908cd847dcb4dc048ea5ceffb3a8a532229d
René Behrens <rene.behrens@aol.com>
2024-08-01 11:08:44 +0200

    Update task

--- Day52_24_07_31_Java_Day26_LiveSession/src/main/java/de/supercode/Main.java
+++ Day52_24_07_31_Java_Day26_LiveSession/src/main/java/de/supercode/Main.java
@@ -15,23 +15,11 @@ public class Main {
             String userInput = scanner.nextLine();
 
             if (userInput.equals("LISTE")) {
-                if (namelist.isEmpty()) {
-                    System.out.println("Keine Namen in der Liste.");
-                } else {
-                    System.out.println("Namen in der Liste:");
-                    for (String name : namelist) {
-                        System.out.println(name);
-                    }
-                }
+                System.out.println(namelist.isEmpty() ? "Keine Namen in der Liste." : "Namen in der Liste:\n" + String.join("\n", namelist));
             } else if (userInput.equals("ENDE")) {
                 break;
             } else {
-                if (!namelist.contains(userInput)) {
-                    namelist.add(userInput);
-                    System.out.println("Gespeichert!");
-                } else {
-                    System.out.println("Dieser Name ist bereits vergeben.");
-                }
+                System.out.println(namelist.add(userInput) ? "Gespeichert!" : "Dieser Name ist bereits vergeben.");
             }
         }
 
