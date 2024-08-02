2b78d9a6ac86ab1541d9f2798aa980149241303e
René Behrens <rene.behrens@aol.com>
2024-08-02 17:32:02 +0200

    Update Main.java

--- Day53_24_08_02_Java_Day27_Alg.Inserionsort/src/main/java/de/supercode/Main.java
+++ Day53_24_08_02_Java_Day27_Alg.Inserionsort/src/main/java/de/supercode/Main.java
@@ -2,6 +2,17 @@ package de.supercode;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("---------------Insertion low to high----------------");
+        int[] list = new int[] {8,3,988,4,2,6,10000,45,57,78,0,64,334,6789,875,789,543,567,8900,7,433,1};
+        displayArray(list);
+        InsertionSort.InsertionSortLowToHigh(list);
+        displayArray(list);
+        System.out.println();
+    }
+
+
+    public static void displayArray(int[] list) {
+        for (int j : list) System.out.print(STR."\{j}, ");
+        System.out.println();
     }
 }
