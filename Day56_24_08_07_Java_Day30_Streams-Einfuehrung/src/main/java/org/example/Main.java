eb02caf560eb49e1172c1f0222af5b701dfb6981
René Behrens <rene.behrens@aol.com>
2024-08-07 15:06:43 +0200

    done Task 2 Stream()-Flatmap

--- Day56_24_08_07_Java_Day30_Streams-Einfuehrung/src/main/java/org/example/Main.java
+++ Day56_24_08_07_Java_Day30_Streams-Einfuehrung/src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -78,6 +79,55 @@ public class Main {
                 .collect(Collectors.toList());
         System.out.println(sortetList);
         //----------------------------------
-        
+
+
+        //!!!!!!! Flatmaps !!!!!!!!
+
+        //1. **Liste von Listen zu einer einzigen Liste “flach machen”**
+        //- Gegeben: Eine Liste von Listen von ganzen Zahlen.
+        //- Aufgabe: Verwende `flatMap`, um eine flache Liste zu erstellen, die alle Zahlen enthält.
+
+        List<List<Integer>> nestedList = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), Arrays.asList(7, 8, 9));
+
+        List<Integer> flatList = nestedList.stream()
+                .flatMap(list -> list.stream())
+                .collect(Collectors.toList());
+        System.out.println(nestedList);
+        System.out.println(flatList);
+        //----------------------------------
+
+        //2. **Zeichenketten in eine Liste von Wörtern aufteilen**
+        //- Gegeben: Eine Liste von Sätzen (Zeichenketten).
+        //- Aufgabe: Verwende `flatMap`, um eine Liste von einzelnen Wörtern zu erstellen.
+
+        List<String> sentenceList = Arrays.asList("Hello world!", "This is a test sentence.", "Programming makes fun.");
+
+        List<String> wordList = sentenceList.stream()
+                .flatMap(s -> Stream.of(s.split(" ")))
+                .collect(Collectors.toList());
+
+        System.out.println(wordList);
+        //----------------------------------
+
+        //3. **Filtern und flach machen einer Liste von Listen**
+        //- Gegeben: Eine Liste von Listen von ganzen Zahlen.
+        //- Aufgabe: Filter alle Zahlen, die größer als 5 sind.
+
+        List<List<Integer>> filteredAndFlattened = nestedList.stream()
+                .map(list -> list.stream()
+                        .filter(n -> n > 5)
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
+
+        System.out.println(filteredAndFlattened);
+
+        //ODER:
+
+        List<Integer> filteredAndFlattened2 = nestedList.stream()
+               .flatMap(list -> list.stream()
+                       .filter(n -> n > 5))
+               .collect(Collectors.toList());
+
+        System.out.println(filteredAndFlattened2);
     }
 }
